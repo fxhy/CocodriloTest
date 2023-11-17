8088b56929137263fc4bebbf2e43f45d00db2e2b
fxhy <hein.fa3@gmail.com>
2023-11-17 09:04:27 +0100

    changed mp3 location

--- src/components/TalkToMe.jsx
+++ src/components/TalkToMe.jsx
@@ -1,3 +1,4 @@
+import sound from "../assets/Monolog.mp3";
 function TalkToMe() {
   // var msg = new SpeechSynthesisUtterance();
   // msg.lang = "es-es";
@@ -10,7 +11,7 @@ function TalkToMe() {
   // msg.text = "Si bien '¡Buenas noches!  Estoy bien, quiero decir, Dios sabe que podría ser peor. Claro que pasa que te encierras en tu habitación Completo con música deprimente y cosas locas. Y todo es sólo segunda o tercera opción como máximo. Entre realmente genial y de alguna manera repugnante como a mitad de camino. No es un secreto ni importante. Pero nunca fui ese punto brillante en el penne El lema del que tomaba notas era: Esperar tiempos mejores para las páginas de lavandería de Otto-Versand. Sólo por eso, el pensamiento de un trabajo y de la vida es la esencia. Desde entonces, mucho más que la pesadilla adolescente promedio Sí, ayuda saber que puedes hacer que la mayoría de los campesinos parezcan viejos como las cosas del prestamista. Pero por lo demás, el tipo probablemente no tenga más que quejas o qué.";
 
   // return window.speechSynthesis.speak(msg);
-  let audio = new Audio("/Monolog.mp3");
+  let audio = new Audio(sound);
   audio.play();
 }
 export default TalkToMe;
